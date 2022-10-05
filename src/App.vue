daf696a8584384a67337c75291d286e936fe63cb
alexandr <xavi08@yandex.ru>
2022-10-05 17:44:17 +0300

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,37 @@
 <template>
   <div class="container">
-      <div class="header">
-        Добавление товара
+    <div class="header">
+      Добавление товара
+    </div>
+    <div class="formCard">
+      <div class="nameCardInput">
+        <p>Наименование товара</p>
+        <input v-model="name" type="text" placeholder="Введите наименование товара" />
       </div>
-      <div class="formCard">
-        <div class="nameCardInput">
-          <p>Наименование товара</p>
-          <input v-model="name" type="text" placeholder="Введите наименование товара"/>
-        </div>
-        <div class="aboutCardInput">
-          <p>Описание товара</p>
-          <input v-model="about" type="text" placeholder="Введите описание товара"/>
-        </div>
-        <div class="imgCardInput">
-          <p>Ссылка на изображение товара</p>
-          <input v-model="image" type="text" placeholder="Ввежите ссылку"/>
-        </div>
-        <div class="priceCardInput">
-          <p>Цена товара</p>
-          <input @keydown.enter="addCard" v-model="price" type="text" placeholder="Ввежите цену"/>
-        </div>
-        <div class="butt">
-          <button @click="addCard" type="buttton">Добавить товар</button>
-        </div>
-        <div>
+      <div class="aboutCardInput">
+        <p>Описание товара</p>
+        <input v-model="about" type="text" placeholder="Введите описание товара" />
+      </div>
+      <div class="imgCardInput">
+        <p>Ссылка на изображение товара</p>
+        <input v-model="image" type="text" placeholder="Ввежите ссылку" />
+      </div>
+      <div class="priceCardInput">
+        <p>Цена товара</p>
+        <input @keydown.enter="addCard" v-model="price" type="text" placeholder="Ввежите цену" />
+      </div>
+      <div class="butt">
+        <button @click="addCard" type="buttton">Добавить товар</button>
+      </div>
+      <div>
 
-        </div>
       </div>
-      
-      <div class="blockCards">
-          <div 
-          v-for="(c, index) in prodactCards"
-          :key="index" 
-          class="productCard">
+    </div>
+
+    <div class="blockCards">
+      <div v-for="(c, index) in prodactCards" :key="index" class="productCard">
         <div class="imgCard">
-          <img :src="c.img"/>
+          <img :src="c.img" />
         </div>
         <div class="nameCard">
           {{c.name}}
@@ -45,10 +42,10 @@
         <div class="priceCard">
           {{c.price}}руб.
         </div>
-       <button @click="removeCard(c)"  class="buttDel" type="button">Удалить</button>
-      </div> 
+        <button @click="removeCard(c)" class="buttDel" type="button">Удалить</button>
       </div>
-      
+    </div>
+
   </div>
 </template>
 
@@ -65,7 +62,7 @@ export default {
       price: "",
       prodactCards: []
     }
-   
+
   },
 
   created() {
@@ -73,7 +70,7 @@ export default {
     if (cardsData) {
       this.prodactCards = JSON.parse(cardsData);
     }
-  },  
+  },
 
   methods: {
     addCard() {
@@ -86,10 +83,10 @@ export default {
 
       this.prodactCards.unshift(newCard);
       localStorage.setItem("cards", JSON.stringify(this.prodactCards));
-     this.image = "";
-     this.name = "";
-     this.about = "";
-     this.price = "";
+      this.image = "";
+      this.name = "";
+      this.about = "";
+      this.price = "";
     },
 
     removeCard(card) {
@@ -109,132 +106,132 @@ export default {
 
 <style>
 .header {
-    grid-area: header;
-    font-family: 'Source Sans Pro';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 28px;
-    line-height: 35px;
-    color: #3F3F3F;
-  }
-  
-  .formCard {
-    grid-area: formCard;
-    width: 332px;
-    height: 440px;
-    background: #FFFEFB;
-    box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
-    border-radius: 4px;
-  }
+  grid-area: header;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 28px;
+  line-height: 35px;
+  color: #3F3F3F;
+}
 
-  .nameCardInput p {
-    position: relative;
-width: 95px;
-height: 13px;
+.formCard {
+  grid-area: formCard;
+  width: 332px;
+  height: 440px;
+  background: #FFFEFB;
+  box-shadow: 0px 20px 30px rgba(0, 0, 0, 0.04), 0px 6px 10px rgba(0, 0, 0, 0.02);
+  border-radius: 4px;
+}
 
-
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 400;
-font-size: 10px;
-line-height: 13px;
-letter-spacing: -0.02em;
-color: #49485E;
-  }
-
-  .nameCardInput input {
-    position: relative;
-width: 284px;
-height: 36px;
+.nameCardInput p {
+  position: relative;
+  width: 95px;
+  height: 13px;
 
 
-background: #FFFEFB;
-box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-border-radius: 4px;
-  }
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
+  letter-spacing: -0.02em;
+  color: #49485E;
+}
+
+.nameCardInput input {
+  position: relative;
+  width: 284px;
+  height: 36px;
+
+
+  background: #FFFEFB;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+}
 
 .aboutCardInput p {
-width: 74px;
-height: 13px;
+  width: 74px;
+  height: 13px;
 
 
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 400;
-font-size: 10px;
-line-height: 13px;
-letter-spacing: -0.02em;
-color: #49485E;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
+  letter-spacing: -0.02em;
+  color: #49485E;
 }
 
 .aboutCardInput input {
-width: 284px;
-height: 108px;
+  width: 284px;
+  height: 108px;
 
-background: #FFFEFB;
-box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-border-radius: 4px;
+  background: #FFFEFB;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
 }
 
 .imgCardInput p {
-width: 134px;
-height: 13px;
+  width: 134px;
+  height: 13px;
 
 
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 400;
-font-size: 10px;
-line-height: 13px;
-letter-spacing: -0.02em;
-color: #49485E;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
+  letter-spacing: -0.02em;
+  color: #49485E;
 
 }
 
 .imgCardInput input {
-width: 284px;
-height: 36px;
+  width: 284px;
+  height: 36px;
 
-background: #FFFEFB;
-box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-border-radius: 4px;
-} 
+  background: #FFFEFB;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+}
 
 .priceCardInput p {
 
-width: 53px;
-height: 13px;
+  width: 53px;
+  height: 13px;
 
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 400;
-font-size: 10px;
-line-height: 13px;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 10px;
+  line-height: 13px;
 
 
-letter-spacing: -0.02em;
+  letter-spacing: -0.02em;
 
-color: #49485E;
+  color: #49485E;
 
 }
 
 .priceCardInput input {
-width: 284px;
-height: 36px;
+  width: 284px;
+  height: 36px;
 
-background: #FFFEFB;
-box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-border-radius: 4px;
-margin-bottom: 20px;
+  background: #FFFEFB;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  margin-bottom: 20px;
 }
 
 .butt button {
-width: 284px;
-height: 36px;
+  width: 284px;
+  height: 36px;
 
-background: #EEEEEE;
-border-radius: 10px;
-cursor:pointer;
+  background: #EEEEEE;
+  border-radius: 10px;
+  cursor: pointer;
 }
 
 
@@ -242,78 +239,78 @@ cursor:pointer;
 
 
 
-  
-  .blockCards{
-    grid-area: blockCards;
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-  }
+
+.blockCards {
+  grid-area: blockCards;
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
 
 
 .imgCard img {
-width: 322px;
-height: 200px;
+  width: 322px;
+  height: 200px;
 
-border-radius: 4px 4px 0px 0px;
-padding-bottom: 10px;
-margin-right: 10px;
+  border-radius: 4px 4px 0px 0px;
+  padding-bottom: 10px;
+  margin-right: 10px;
 }
 
 .nameCard {
-width: 201px;
-height: 25px;
+  width: 201px;
+  height: 25px;
 
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 600;
-font-size: 20px;
-line-height: 25px;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 25px;
 
-color: #3F3F3F;
-padding-bottom: 10px;
+  color: #3F3F3F;
+  padding-bottom: 10px;
 
 }
 
 .aboutCard {
-width: 300px;
-height: 80px;
+  width: 300px;
+  height: 80px;
 
 
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 20px;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 20px;
 
-color: #3F3F3F;
-padding-bottom: 10px;
-word-wrap: break-word;
+  color: #3F3F3F;
+  padding-bottom: 10px;
+  word-wrap: break-word;
 }
 
 .priceCard {
-width: 117px;
-height: 30px;
+  width: 117px;
+  height: 30px;
 
-font-family: 'Source Sans Pro';
-font-style: normal;
-font-weight: 600;
-font-size: 24px;
-line-height: 30px;
+  font-family: 'Source Sans Pro';
+  font-style: normal;
+  font-weight: 600;
+  font-size: 24px;
+  line-height: 30px;
 
-color: #3F3F3F;
-padding-bottom: 10px;
+  color: #3F3F3F;
+  padding-bottom: 10px;
 }
 
 .buttDel {
   position: relative;
   width: 84px;
-height: 26px;
+  height: 26px;
 
-background: #EEEEEE;
-border-radius: 10px;
+  background: #EEEEEE;
+  border-radius: 10px;
   margin-bottom: 10px;
-  cursor:pointer;
+  cursor: pointer;
 }
 
 
@@ -321,18 +318,13 @@ border-radius: 10px;
 
 
 
-  
-  .container {
-    display: grid;
-    grid-template-areas: 
-            'header header'
-            'formCard blockCards';
-    
-    grid-template-columns: 332px 1fr;
-  }
-  
-  
-  
-  
 
+.container {
+  display: grid;
+  grid-template-areas:
+    'header header'
+    'formCard blockCards';
+
+  grid-template-columns: 332px 1fr;
+}
 </style>
